8f6f7d352957a84357abbf9b7a46b125f6cbc343
Huy Cuong <huycuong2912001@gmail.com>
2023-01-02 19:58:02 +0700

    no message

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import cuong from "../../assets/cuong.png"
-import truong from "../../assets/truong.png"
-import giang from "../../assets/giang.png"
-import img from "../../img/1.png"
+import cuong from "../../assets/cuong.png";
+import truong from "../../assets/truong.png";
+import giang from "../../assets/giang.png";
+import img from "../../img/1.png";
 
 import { themeContext } from "../../Context";
 const Portfolio = () => {
@@ -14,26 +14,30 @@ const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{ color: darkMode ? 'white' : '' }}>About</span>
+      <span style={{ color: darkMode ? "white" : "" }}>About</span>
       <span>Our Instructor</span>
-          <img src={img} alt="" style={{marginBottom:"50px",marginTop:"30px"}}/>
+      <a href="https://www.facebook.com/ntbaovn" target="_blank">
+        <img src={img} alt="" style={{ marginBottom: "50px", marginTop: "30px" }} />
+      </a>
+
       <span> Our Team</span>
 
       {/* slider */}
-      <Swiper
-        spaceBetween={300}
-        slidesPerView={3}
-        grabCursor={true}
-        className="portfolio-slider"
-      >
+      <Swiper spaceBetween={300} slidesPerView={3} grabCursor={true} className="portfolio-slider">
         <SwiperSlide>
-          <img src={cuong} alt="" />
+          <a href="https://www.facebook.com/huycuong291/" target="_blank">
+            <img src={cuong} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={giang} alt="" />
+          <a href="https://www.facebook.com/giang.dnt.cod" target="_blank">
+            <img src={giang} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={truong} alt="" />
+          <a href="https://www.facebook.com/dantruong.cun" target="_blank">
+            <img src={truong} alt="" />
+          </a>
         </SwiperSlide>
       </Swiper>
     </div>
