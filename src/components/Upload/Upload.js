50f43a23c218810667c7b0e2f5ca6e680d777607
Huy Cuong <cuong@atechpartner.com>
2023-05-06 22:40:19 +0700

    update link backend

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,25 +1,23 @@
-
 import React, { useState } from "react";
 import { useEffect } from "react";
 import DropFileInput from "../drop-file-input/DropFileInput";
 import DetectAndShow from "./DetectAndShow/DetectAndShow";
-import './styles.css'
+import "./styles.css";
 import axios from "axios";
 import { themeContext } from "../../Context";
 import { useContext } from "react";
-import { LoadingOutlined } from '@ant-design/icons';
-import { Spin } from 'antd';
-import { Skeleton } from 'antd';
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+import { Skeleton } from "antd";
 
 function UploadFile() {
   const [listImageBase64, setListImageBase64] = useState([]);
-  const [listImage, setListImage] = useState([])
-  const [dataResponse, setDataResponse] = useState([])
-  const [isDetected, setDetected] = useState(false)
+  const [listImage, setListImage] = useState([]);
+  const [dataResponse, setDataResponse] = useState([]);
+  const [isDetected, setDetected] = useState(false);
 
-  const [loading, setLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
-
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const convertBase64 = (file) => {
     return new Promise((resolve, reject) => {
@@ -34,49 +32,50 @@ function UploadFile() {
     });
   };
   useEffect(async () => {
-    let temp = []
+    let temp = [];
     for (let i = 0; i < listImage.length; i++) {
-      temp.push(await convertBase64(listImage[i]))
+      temp.push(await convertBase64(listImage[i]));
     }
-    temp = [...temp, ...listImageBase64]
+    temp = [...temp, ...listImageBase64];
     setListImageBase64(temp);
-  }, [listImage])
+  }, [listImage]);
 
   const uploadAndDetect = () => {
-    setIsError(false)
-    setLoading(true)
-    listImageBase64.length ? listImageBase64.forEach(item => {
-      axios.post("https://interior-object-detect-backend-production.up.railway.app/detect", { base64: item }).then(function (response) {
-        console.log(response)
-        if (response.data !== "ERROR") {
-          setDetected(true)
-          setLoading(false)
-          const arrResponse = Object.keys(response.data).map((key) => response.data[key]);
-          setDataResponse([...arrResponse])
-        }
-        else {
-          setDetected(true)
-          setLoading(false)
-          setIsError(true)
-        }
-      })
-    }) : setLoading(false)
+    setIsError(false);
+    setLoading(true);
+    listImageBase64.length
+      ? listImageBase64.forEach((item) => {
+          axios.post("https://web-production-68e5.up.railway.app/detect", { base64: item }).then(function (response) {
+            console.log(response);
+            if (response.data !== "ERROR") {
+              setDetected(true);
+              setLoading(false);
+              const arrResponse = Object.keys(response.data).map((key) => response.data[key]);
+              setDataResponse([...arrResponse]);
+            } else {
+              setDetected(true);
+              setLoading(false);
+              setIsError(true);
+            }
+          });
+        })
+      : setLoading(false);
 
-    setListImageBase64([])
-    setListImage([])
-  }
+    setListImageBase64([]);
+    setListImage([]);
+  };
   const fileChangeHandle = async (files) => {
-    setIsError(false)
-    await setListImage(files)
-    setDetected(false)
-  }
+    setIsError(false);
+    await setListImage(files);
+    setDetected(false);
+  };
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   const antIcon = (
     <LoadingOutlined
       style={{
         fontSize: 24,
-        color: "#FCA61F"
+        color: "#FCA61F",
       }}
       spin
     />
@@ -84,32 +83,37 @@ function UploadFile() {
 
   return (
     <>
-      <div className="main-upload" id="main-upload" style={{backgroundColor:darkMode&& "lightslategrey"}}>
-        <span style={{ color: darkMode ? 'white' : '' }}>Upload image file</span>
+      <div className="main-upload" id="main-upload" style={{ backgroundColor: darkMode && "lightslategrey" }}>
+        <span style={{ color: darkMode ? "white" : "" }}>Upload image file</span>
         <span>And Detect Now!</span>
         <div className="box-upload">
-          <DropFileInput
-            onFileChange={fileChangeHandle}
-            isDetected={isDetected}
-          />
-          {loading ? <>
-            <Skeleton
-              active
-              avatar
-              paragraph={{
-                rows: 4,
-              }} />
-          </>
-            :
+          <DropFileInput onFileChange={fileChangeHandle} isDetected={isDetected} />
+          {loading ? (
             <>
-              {isError && <p style={{ marginTop: "50px", fontSize: "24px", color: "red", fontWeight: "bold" }}>No object detected</p>}
-              <button className="button i-button" onClick={uploadAndDetect}>Upload and detect</button>
-            </>}
+              <Skeleton
+                active
+                avatar
+                paragraph={{
+                  rows: 4,
+                }}
+              />
+            </>
+          ) : (
+            <>
+              {isError && (
+                <p style={{ marginTop: "50px", fontSize: "24px", color: "red", fontWeight: "bold" }}>
+                  No object detected
+                </p>
+              )}
+              <button className="button i-button" onClick={uploadAndDetect}>
+                Upload and detect
+              </button>
+            </>
+          )}
         </div>
       </div>
 
       {isDetected && !isError && <DetectAndShow darkMode={darkMode} data={dataResponse}></DetectAndShow>}
-
     </>
   );
 }
